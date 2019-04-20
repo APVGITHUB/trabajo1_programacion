8f61612a3f518d5d908306b45c63ceeaa3a58f25
APVGITHUB <alex.pr434@gmail.com>
2019-04-20 22:37:17 +0200

    Añado archivo del main. Esta a medias pero es para ir preparandolo todo

---
+++ trabajops.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+#include<obtener.h>
+#include<algos.h>
+#include<chrono>
+#define NMAT //NUMERO DE MATRICES DE PRUEBA
+using namespace std;
+
+int main(){
+
+  int** p;
+  int time[2];//tiempo que tarda cada algo en resolver
+  int optimoalg1[NMAT];
+  int optimoalg2[NMAT];
+  //declaración de matrices//
+  **p=Crea_Nodos(1);
+
+
+
+    optimoalg1[0]=solve(**p);//¿valor o dirección?
+    t2=
+}
+  //uso de algoritmos//
+
+
+
+  getch();
+  return 0;
+}
+
