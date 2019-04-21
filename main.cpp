4b695fa74d222bb669ab22d8b809d0e8dd0a25d2
guillee35 <49530604+guillee35@users.noreply.github.com>
2019-04-21 21:02:38 +0200

    Update main.cpp
    compila

--- main.cpp
+++ main.cpp
@@ -1,27 +1,19 @@
-#include <stdio.h>
 #include <iostream>
-#include <stdlib.h>
+#include <conio.h>
 #include "matrix.h"
 
 using namespace std;
 
-int **m_matrix;
+
 
 int main(){
-
-	extern int **m_matrix;
-	m_matrix=(int **)malloc(10*sizeof(int *));
-	for(int i=0;i<10;i++){
-		m_matrix[i]=(int *)malloc(10*sizeof(int));
-	}//DECLARACION DE PUNTERO Y RESERVA DE MEMORIA
-		//MATRIZ MAXIMA 10X10
-
 	int ejemplo=0;
-	cout<<"elija un ejemplo de matriz: ";
+	cout<<"Elija un experimento: ";
 	cin>>ejemplo;
-
+	cout << endl;
 	Matrix q= Matrix(ejemplo);
 	q.fill_matrix();
 	q.print_matrix();
+	/* faltaria liberar la memoria del puntero al final del programa */
 	return 0;
 }
