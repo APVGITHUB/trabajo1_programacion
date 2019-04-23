680716f6e5d9e07460467d20cf32db16092b32d9
jdcdp <47483528+jdcdp@users.noreply.github.com>
2019-04-23 19:07:43 +0200

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,35 +1,18 @@
 #include <iostream>
-#include <conio.h>
-#include <time.h>
-#include "matrix.h"
-#define DIM 10			/* dimension maxima del espacio reservado para la matriz */
+#include <time.h>	
+#define DIM 10	/* dimension maxima del espacio reservado para la matriz */
 #define x 9				/* distancia maxima entre puntos */
 using namespace std;
 
+int exp=0;
+int m_matrix[DIM][DIM];
+int m=DIM;
 
 
-Matrix::Matrix(int _exp){		/* constructor */
-  exp=_exp;
-  m_matrix=(int **)malloc(DIM*sizeof(int *));
-	for(int i=0;i<DIM;i++){
-		m_matrix[i]=(int *)malloc(DIM*sizeof(int));
-	}
-}
-
-
-Matrix::~Matrix() {}			/* destructor */
-
-
-
-void Matrix::set_dimension(int m){	/* dimension de la matriz */
-	N=m;
-}
-
-
-void Matrix::print_matrix(){		/* imprime la matriz */
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout <<*(*(m_matrix+i)+j)<< " ";
+void print_matrix(){		/* imprime la matriz */
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            cout <<m_matrix[i][j]<< " ";
         }
         cout << endl;
     }
@@ -38,8 +21,8 @@ void Matrix::print_matrix(){		/* imprime la matriz */
 
 
 
-void Matrix::fill_matrix(){		/* llena la matriz segun el experimento */ 
-	int m;
+void fill_matrix(){		/* llena la matriz segun el experimento */ 
+
   int a[2][2]={{1,2},
                {3,4}};
 
@@ -47,39 +30,34 @@ void Matrix::fill_matrix(){		/* llena la matriz segun el experimento */
                {4,5,6},
                {7,8,9}};
 
-	switch (exp){
+	switch (2){
 		case 0: m=2;
-			set_dimension(m);
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
 					m_matrix[i][j] =a[i][j];
 				}
 			}
 			break;
 		case 1: m=3;
-			set_dimension(m);
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
 					m_matrix[i][j] =b[i][j];
 				}
 			}
 			break;
 		default:		/* experimento aleatorio -> genera matriz aleatoria */
-			cout << "Numero de puntos: ";  /* dimension de la matriz */
-			cin >> m;
-			set_dimension(m);
 			cout << endl;
 			srand(time(NULL));
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++){
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++){
 					if (i==j) 
 						m_matrix[i][j] = 0;
 					else 
 						m_matrix[i][j] = rand()%(x+1);
 				}
 			}
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++){
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++){
 					m_matrix[i][j] = m_matrix[j][i];
 				}
 			}
