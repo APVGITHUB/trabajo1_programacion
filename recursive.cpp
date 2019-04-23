6d7140e071c29cbe63f47232b75b279110a02da5
Jdcdp <jaimedecastrop@gmail.com>
2019-04-23 16:12:47 +0200

    Bugfixes recursive.cpp

--- recursive.cpp
+++ recursive.cpp
@@ -1,8 +1,7 @@
-#include "matrix.cpp"
 #define INF 1000000000
 
 
-extern int m_matrix[][];/* m_matrix[i][j] es la distancia de i a j */
+extern int m_matrix[][];
 extern int path[];
 const int n=sizeof(m_matrix)/sizeof(m_matrix[0]);
 int i,temp=-1;
@@ -13,47 +12,52 @@ class node{
 	 int father=-1;
 	public:
 	 int number;
+	 int get_father(){
+	 	return father;
+	 }
 	 int get_length(){
 	 	if(length!=-1){/*esto es para no tener que recalcular la distancia en cada llamada al nodo*/
 		length=find_shortest(number);
-		father=temp
+		father=temp;
 		}
 		return number;
 	 }
-	  int init(){/*define el nodo de inicio*/
+	 void init(){/*define el nodo de inicio*/
 	  	length=0;
 	  }
 };
 
 node node[n];
 
-void find_shortest(int k){/*k es el nodo que se esta evaluando*/
+int find_shortest(int k){/*k es el nodo que se esta evaluando*/
   int curr_length=INF;
   int node_length;
-  int distance
+  int distance;
   for (i=0; i<n; i++){
 	if(distance=m_matrix[k][i]){ /*asumo que las posiciones de la diagonal estan vacias*/
 		node_length=node[i].get_length();
-		if(node_length+distance)<curr_length){
+		if((node_length+distance)<curr_length){
 			curr_length=node_length+distance;
 			temp=i;
 		}
 	}
+  }
+  return curr_lenght;
 }
 void calcular_rec(){
 
 	for(i=0;i<n;i++){
 	node[i].number=i;
 	}
-	node[0].init;
+	node[0].init();
 
-	find_shortest(n); //calculo
+	int l=find_shortest(n); //calculo
 
-	i=n;
-	k=end;
+	i=0;
+	int k=n;
 	do { /* Aqui se obtiene el vector que indica el camino correcto */
 		path[i]=k;
-		k = node[k].father;
+		k = node[k].get_father();
 		i++;
 	} while (k > 0);
 }
