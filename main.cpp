2e1650e347dcebcfb4ae0a704e62e06363b40bb4
jdcdp <47483528+jdcdp@users.noreply.github.com>
2019-04-23 19:41:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include "dijkstra.cpp"
+#include "recursive.cpp"
 #include <time.h>
 #include <chrono>
 using namespace std;
 
 int main(){
-	double time_taken=0;
-	clock_t start,end;
-	
-
-	int ejemplo=0;
+	int ejemplo=3;
 	cout<<"Elija un experimento: ";
 	cin>>ejemplo;
 	cout << endl;
 	fill_matrix();
 	print_matrix();
-	auto start= chrono::steady_clock::now()
-	calcular_dij(0,9);
-	auto end= chrono::steady_clock::now()
+//test 1
+	auto start= chrono::steady_clock::now();
+	calcular_dij(0,DIM-1);
+	auto end= chrono::steady_clock::now();
 	double elapsed_time = double(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
-	cout << elapsed_time << "nanoseconds " << endl;
+	cout << elapsed_time << " " << "nanoseconds in test 1" << endl;
+//test 2
+	auto start= chrono::steady_clock::now();
+        calcular_rec(0,DIM-1);
+        auto end= chrono::steady_clock::now();
+        double elapsed_time = double(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
+        cout << elapsed_time << " " << "nanoseconds in test 2" << endl;
+
 	return 0;
 }
